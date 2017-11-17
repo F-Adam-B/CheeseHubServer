f9bbed60721b0ce8dc439244c04c2b6eca81addd
Adam Bradbury <f.adambrad@gmail.com>
2017-11-17 16:18:52 -0700

    pushing to github prior to heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
@@ -20,28 +23,30 @@ app.use(
     })
 );
 
-app.get('/api/cheeses', (req, res) => {
-    res.json(
-        [
-            "Bath Blue",
-            "Barkham Blue",
-            "Buxton Blue",
-            "Cheshire Blue",
-            "Devon Blue",
-            "Dorset Blue Vinney",
-            "Dovedale",
-            "Exmoor Blue",
-            "Harbourne Blue",
-            "Lanark Blue",
-            "Lymeswold",
-            "Oxford Blue",
-            "Shropshire Blue",
-            "Stichelton",
-            "Stilton",
-            "Blue Wensleydale",
-            "Yorkshire Blue"
-        ]
-    )
+app.get('/api/cheeses', jsonParser, (req, res) => {
+    
+    const cheeses = [
+        "Bath Blue",
+        "Barkham Blue",
+        "Buxton Blue",
+        "Cheshire Blue",
+        "Devon Blue",
+        "Dorset Blue Vinney",
+        "Dovedale",
+        "Exmoor Blue",
+        "Harbourne Blue",
+        "Lanark Blue",
+        "Lymeswold",
+        "Oxford Blue",
+        "Shropshire Blue",
+        "Stichelton",
+        "Stilton",
+        "Blue Wensleydale",
+        "Yorkshire Blue"
+    ]
+     
+    console.log('working')
+    res.json(cheeses);
 })
 
 function runServer(port = PORT) {
